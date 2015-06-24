9e90be8d9aa2feac795b2d321ffb95831354493b
DENGSHUOCEHNG <983281448@qq.com>
2015-06-24 19:52:50 +0800

    no message

---
+++ 学生管理系统/stulist01/initdb.py
@@ -0,0 +1,47 @@
+﻿#! /usr/bin/env python3
+# -*- coding:UTF-8 -*-
+
+from dbconn import db_cursor
+
+def create_db():
+    sqlstr = """
+    DROP TABLE IF EXISTS course;
+
+    CREATE TABLE IF NOT EXISTS course  (
+        cou_sn   INTEGER,     --序号
+        cou_no   TEXT,        --课程号
+        name     TEXT,        --课程名称
+        notes    TEXT,
+        PRIMARY KEY(cou_sn)
+    );
+    -- CREATE UNIQUE INDEX idx_course_no ON course(cou_no);
+
+    CREATE SEQUENCE seq_cou_sn 
+        START 10000 INCREMENT 1 OWNED BY course.cou_sn;
+
+    """
+    with db_cursor() as cur :
+        cur.execute(sqlstr) # 执行SQL语句
+    
+def init_data():
+    sqlstr = """
+    DELETE FROM course;
+
+    INSERT INTO course (cou_sn, cou_no, name)  VALUES 
+        (101, '1310650306',  '邓硕成'), 
+        (102, '1310650309',  '于振儒'),
+        (103, '1310650325',  '杨新蕊'),
+        (104, '1310650326',  '崔佳琪'),
+        (105, '1310650327',  '李  杰');
+     
+
+
+    """
+    with db_cursor() as cur :
+        cur.execute(sqlstr)    
+
+if __name__ == '__main__':
+    create_db()
+    init_data()
+    print('数据库已初始化完毕！')
+
